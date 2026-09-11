a3350f0c3db5e12380f0b9c674b8da3529da2f78
Syed AliAkbar Rizvi <akbar.ali576@gmail.com>
2026-09-11 14:39:41 -0700

    Create App component with initialization message

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-black text-white font-mono flex items-center justify-center">
+      <p className="text-2xl uppercase tracking-widest">
+        [ SYSTEM INITIALIZED _ ]
+      </p>
+    </div>
+  );
+}
