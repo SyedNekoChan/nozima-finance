fe1f205b381a3eb92a47bc32af1cef2626dedb81
Syed AliAkbar Rizvi <akbar.ali576@gmail.com>
2026-09-25 13:20:55 -0700

    Add global definition and buffer alias to config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/nozima-finance/',
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
 })
